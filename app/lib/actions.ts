445ead10dd90410b2a59cafe100da13b4b2c233c
l_mikhail <l_mikhail@inbox.ru>
2024-10-11 10:56:25 +0300

    Chapter 11. CRUD

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,7 @@
 import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
 const FormSchema = z.object({
   id: z.string(),
@@ -31,24 +31,30 @@ export async function createInvoice(formData: FormData) {
 `;
 
   revalidatePath("/dashboard/invoices");
-  redirect('/dashboard/invoices');
+  redirect("/dashboard/invoices");
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
-      customerId: formData.get('customerId'),
-      amount: formData.get('amount'),
-      status: formData.get('status'),
-    });
-   
-    const amountInCents = amount * 100;
-   
-    await sql`
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
       UPDATE invoices
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
     `;
-   
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
-  }
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  
+  revalidatePath("/dashboard/invoices");
+}
